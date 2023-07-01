c89b048d3b1c80011cea1c4c7f161dd1c7bbb610
ZeberMVP <rubeen722@gmail.com>
2023-07-01 19:24:16 +0200

    chore: Setup app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,14 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+
+import type { Metadata } from 'next'
 import { ClerkProvider } from '@clerk/nextjs'
 
-const inter = Inter({ subsets: ['latin'] })
+import { fontMono, fontSans } from '@/lib/fonts'
+import { cn } from '@/lib/utils'
+import { Toaster } from '@/components/ui/Toaster'
+import { TailwindIndicator } from '@/components/TailwindIndicator'
 
-export const metadata = {
+export const metadata: Metadata = {
 	title: 'NUDE PROJECT | By artists, for artists.',
 	description:
 		'Nude Project es una marca de prêt-à-porter fundada en 2018 en un pequeño dormitorio por dos amigos con ganas de crear algo diferente. Nuestras prendas pretenden inspirar a la nueva generación a perseguir su pasión, sea o no el camino tradicional, empoderando a los artistas para que creen sin miedo a ser juzgados.',
@@ -17,8 +21,19 @@ export default function RootLayout({
 }) {
 	return (
 		<ClerkProvider>
-			<html lang='en'>
-				<body className={inter.className}>{children}</body>
+			<html lang='en' suppressHydrationWarning>
+				<head />
+				<body
+					className={cn(
+						'min-h-screen bg-background font-sans antialiased',
+						fontSans.variable,
+						fontMono.variable
+					)}
+				>
+					{children}
+					<TailwindIndicator />
+					<Toaster />
+				</body>
 			</html>
 		</ClerkProvider>
 	)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import Logo from '@/components/ui/Logo'
 import { UserButton } from '@clerk/nextjs'
 
 export default function Home() {
 	return (
 		<main className='flex min-h-screen flex-col items-center justify-between py-10 px-24'>
-			<Logo className='text-yellow-950 fill-current' />
 			<UserButton afterSignOutUrl='/' />
 		</main>
 	)
